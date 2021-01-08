f258c9a75e5fb7e7d262b2382539b7df969318ef
Satish Kumar <satish.kumar06@nagarro.com>
2021-01-08 19:40:15 +0530

    user authentication done
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,26 @@
 import { Router } from 'express'
+import { authByToken } from '../middlewares/auth'
+import { getUserByEmail } from "../controllers/users"
 
 const route = Router()
 
-// GET /user
-route.get('/', (req, res) => {
-    res.send('GET /user has been hit') //TODO: temporary
+// GET /user           get current user
+route.get('/', authByToken, async (req, res) => {
+    
+    try {
+      const user = await getUserByEmail((req as any).user.email)
+      if (!user) throw new Error('No such user found')
+      return res.status(200).json({user})
+    } catch (e) {
+      return res.status(404).json({
+        errors: { body: [ e.message ] }
+      })
+    }
+  
+  })
+
+route.patch('/', authByToken, async (req, res) => {
+
 })
 
 export const userRoute = route
